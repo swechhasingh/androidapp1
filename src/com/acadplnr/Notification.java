afec8562739b071eb71ecf341a973a6bcb113269
kundan2510 <kundan@iitk.ac.in>
2013-06-09 12:14:43 +0530

    html data recieved from given url

--- src/com/acadplnr/Notification.java
+++ src/com/acadplnr/Notification.java
@@ -1,15 +1,34 @@
 package com.acadplnr;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Notification extends Activity{
-
+	TextView HttpStuff;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.notification);
+		HttpStuff = (TextView) findViewById(R.id.tvHttp);
+		GetMethodEx test = new GetMethodEx();
+		String returned;
+		
+		try{
+			returned = test.getInternetData();
+			HttpStuff.setText(returned);
+		}catch(Exception e){
+			e.printStackTrace();
+			String error = e.toString();
+			Dialog h = new Dialog(this);
+			h.setTitle(" :(");
+			TextView tv1 = new TextView(this);
+			tv1.setText(error);
+			h.setContentView(tv1);
+			h.show();
+		}
 		
 	}  
 }
